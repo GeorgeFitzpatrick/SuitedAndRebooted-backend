0be5d07950a62320bbab4a2103a1185e8edf48d0
George <george-fitzpatrick@outlook.com>
2024-12-03 11:13:07 +0000

    create-audio-upload-endpoint

--- src/main/java/com/suited/demo/Controller.java
+++ src/main/java/com/suited/demo/Controller.java
@@ -1,7 +1,16 @@
 package com.suited.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 /**
  * @author George Fitzpatrick
@@ -9,9 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("/")
-    public String greeting() {
-        return "Hello World";
+    @PostMapping("/")
+    public ResponseEntity<String> handle(@RequestParam(value = "file", required = false) MultipartFile file) throws IOException {
+        if (file == null || file.isEmpty()) {
+            return ResponseEntity.status(BAD_REQUEST).body("No file uploaded");
+        }
+
+        byte[] bytes = file.getBytes();
+        return ResponseEntity.ok("File uploaded successfully: " + file.getOriginalFilename());
     }
 
 }
